9e57f0979ed6df1d3528c043ec49b7487b0786e5
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-02 14:27:04 -0800

    Add in contact form mailto link

---
+++ app/public/javascript/asset.js
@@ -0,0 +1,8 @@
+
+$("#submit-button").on("click", function(event){
+    event.preventDefault();
+    
+    location.href = "mailto:shelbyreins@yahoo.com?subject=" + $("#subject").val() + "&body=" + $("#form-textarea").val();
+    
+  
+})
