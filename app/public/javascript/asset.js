bb7ff2e25210f1ea7157de754726ddbb97d497ec
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-02 21:42:17 -0800

    add target _blank attribute to projects and media links

--- app/public/javascript/asset.js
+++ app/public/javascript/asset.js
@@ -4,5 +4,8 @@ $("#submit-button").on("click", function(event){
     
     location.href = "mailto:shelbyreins@yahoo.com?subject=" + $("#subject").val() + "&body=" + $("#form-textarea").val() + "";
     
-  
+    $("#name").val("");
+    $("#subject").val("");
+    $("#email").val("");
+    $("#form-textarea").val("");
 })
